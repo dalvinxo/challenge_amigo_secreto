2b6321c04fb15a6f862bdfc6ff32f624d76b9eb0
dalvinxo <dalvingamer@gmail.com>
2025-02-16 15:27:19 -0400

    creando funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+function asignarAmigoALista() {
 
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML ='';
 
+    for(let amigo of amigos) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+
+}
 
 function agregarAmigo() {
+    
     inputNombre = document.querySelector("#amigo");
     nombreAmigo = inputNombre.value;
 
@@ -16,4 +27,9 @@ function agregarAmigo() {
     amigos.push(nombreAmigo);
     inputNombre.value = '';
 
+    asignarAmigoALista();
+
 }
+
+
+
