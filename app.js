ac87bc68299581ad762de2aacd3a9b59c25cf6ce
dalvinxo <dalvingamer@gmail.com>
2025-02-16 15:35:41 -0400

    function para mostrar amigo sorteado

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+function sortearAmigo() {
+
+    if(amigo.length == 0) {
+       alert("No hay amigos para sortear.");
+       return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigo.length);
+    let amigoSorteado = amigo[indiceAleatorio];
+
+    let ElementoResultado = document.getElementById('resultado');
+    ElementoResultado.innerHTML = amigoSorteado;
+
+}
+
 function asignarAmigoALista() {
 
     let lista = document.querySelector('#listaAmigos');
